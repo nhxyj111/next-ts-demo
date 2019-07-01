594f63c35061a6f3de5d9f03352adff9effb91f6
YeJ XU <nhxyj111@gmail.com>
2019-07-01 12:01:11 +0800

    withRouter

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,16 @@
 import Layout from "../components/Layout"
 import Link from 'next/link'
 
-const PostLink: React.SFC<{ title: string }> = ({ title }) => (
-  <li><Link href={`/post?title=${title}`}><a>{title}</a></Link></li>
+const PostLink: React.SFC<{ title: string, slug: string }> = ({ title, slug }) => (
+  <li><Link as={slug} href={`/post?title=${title}`}><a>{title}</a></Link></li>
 )
 
 const Blog = () => (
   <Layout title="My Blog">
     <ul>
-      <PostLink title="react" />
-      <PostLink title="angular" />
-      <PostLink title="vue" />
+      <PostLink slug="react-post" title="react post" />
+      <PostLink slug="angular-post" title="angular post" />
+      <PostLink slug="vue-post" title="vue post" />
     </ul>
   </Layout>
 )
--- pages/post.tsx
+++ pages/post.tsx
@@ -1,9 +1,10 @@
 import Layout from "../components/Layout";
+import { withRouter } from 'next/router'
 
-const Post: React.SFC<{ url: any }> = ({ url }) => (
-  <Layout title={url.query.title}>
+const Post: React.SFC<{ router: any }> = ({ router }) => (
+  <Layout title={router.query.title}>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum harum assumenda tempora beatae minima consequuntur aliquam eligendi doloremque optio dolorem architecto facilis. Ut, vero est nobis doloremque quae blanditiis?</p>
   </Layout>
 )
 
-export default Post
+export default withRouter(Post)
